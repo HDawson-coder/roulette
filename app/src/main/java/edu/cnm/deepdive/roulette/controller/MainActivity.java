73157afa11cac0568fa7287ce6e8f1f269d702f2
Hana Dawson <stubbornunicorn95@gmail.com>
2021-02-24 14:50:46 -0700

    Added statistics view in DB, and started fragment.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     BottomNavigationView navView = findViewById(R.id.nav_view);
     appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.navigation_play, R.id.navigation_dashboard, R.id.navigation_notifications)
+        R.id.navigation_play, R.id.navigation_history, R.id.navigation_statistics)
         .build();
     navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);

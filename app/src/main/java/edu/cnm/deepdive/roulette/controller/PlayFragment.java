e0507957986e3da924706973ee358732a7d7bc1a
Hana Dawson <stubbornunicorn95@gmail.com>
2021-03-02 14:23:13 -0700

    Fixed animation artifact.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/PlayFragment.java
@@ -77,6 +77,12 @@ public class PlayFragment extends Fragment {
     });
   }
 
+  @Override
+  public void onStop() {
+    binding.rouletteWheel.clearAnimation();
+    super.onStop();
+  }
+
   private void rotateToPocket(Integer pocketIndex) {
     float finalRotation =
         -DEGREES_PER_REVOLUTION * pocketIndex / (float) PlayViewModel.POCKETS_ON_WHEEL;
